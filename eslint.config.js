ae3769c65f8453139b9def99101fc6d725cd02a7
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-11-24 23:54:37 +0100

    chore(config) setup eslint, prettier, husky and commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +16,42 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1. React
+            ['^react$', '^react-dom$'],
+
+            // 2. Librerías externas
+            ['^@?\\w'],
+
+            // 3. Imports internos del proyecto (alias @/ o src/)
+            ['^@/', '^src/'],
+
+            // 4. Rutas relativas (módulos propios)
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'], // parent
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'], // sibling & index
+
+            // 5. Tipos (si usas una carpeta de tipos, por ejemplo @/types)
+            ['^@/types'],
+
+            // 6. Estilos (css/scss) SIEMPRE al final
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
