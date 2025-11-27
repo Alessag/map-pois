662cfea7d5a6a0d46de71b55971d9b61a52ea26d
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2025-11-27 14:08:25 +0100

    chore(config): add vitest and react testing library

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    css: true,
+    clearMocks: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/main.tsx',
+        'src/services/situm.service.ts',
+        'src/**/*.d.ts',
+        'src/**/types.ts',
+        'src/**/constants.ts',
+      ],
+    },
+  },
+});
